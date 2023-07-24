628bac4e7da95295cf374b65462a5ef4323ffeaf
Satalae <harrison19988@gmail.com>
2023-07-23 18:44:23 -0700

    database works properly now, install needs work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,9 +16,9 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.log('PUT accepted content into db');
   const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transation('jate', 'readwrite');
+  const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ jate: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('Jate saved to database!', result);
@@ -28,13 +28,13 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET All content from the db.');
   const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transation('jate', 'readonly');
+  const tx = jateDB.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
